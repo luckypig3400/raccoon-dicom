9c7d32312ecf3b8c0188dd715245e70e4da56b2f
郵差 <twHenryYang@gmail.com>
2023-07-03 11:35:45 +0800

    feat: Set build option for deploying in any path
    https://vitejs.dev/guide/static-deploy.html#github-pages

--- raccoon2-ui/vite.config.js
+++ raccoon2-ui/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: "./",
 })
